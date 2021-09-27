1aaf46eec8c9030ae9b79c5184a663cf50efcf44
abhilash6398 <abhilash6398@gmail.com>
2021-09-27 12:59:49 +0530

    updated-profile

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -4,12 +4,18 @@ import profile_img from '../assets/images/profile_img.jpg';
 function Profile() {
     return (
         <div className="Container">
-            <div className="row">
+            <div className="row" style={{backgroundColor:"lightGray", margin: "20px 400px" }}>
                 <div className="form-group col-auto col-md-4">
-                <img src={profile_img} class="rounded-circle" alt="Cinque Terre" style={{height:"100px",marginLeft:"350px",marginTop:"30px"}}/>
+                <img src={profile_img} class="rounded-circle" alt="Cinque Terre" style={{height:"100px",marginLeft:"30px",marginTop:"15px"}}/>
                 </div>
-                <div className="form-group col-auto col-md-4">
-                <label>Name</label>
+                <br/>
+                <div className="form-group col-auto col-md-4" style={{marginTop:"20px"}}>
+                <label className="text-muted">Name</label>
+                <p>Abhilash Sharma</p>
+                </div>
+                <div className="form-group col-auto col-md-4" style={{marginTop:"20px"}}>
+                <label className="text-muted">Phone Number</label>
+                <p>+91-8586016209</p>
                 </div>
             </div>
         </div>
